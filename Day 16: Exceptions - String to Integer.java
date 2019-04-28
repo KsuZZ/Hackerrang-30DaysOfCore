5e63cf2308c7ca87f0701370d3d729f0d39958df
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2019-04-28 23:07:21 +0300

    Create Day 16: Exceptions - String to Integer.java

---
+++ Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int number = 0;
+        try {
+            number = Integer.parseInt(S);
+            System.out.println(number);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+    }
+}
